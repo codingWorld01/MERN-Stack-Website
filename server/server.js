70e109105cebe8709c4806e9a0730db901ba3344
Yatharth Aurangpure <106876791+codingWorld01@users.noreply.github.com>
2024-09-29 12:28:09 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const path = require('path');
 const URL = "servicenow-backend.vercel.app";
 
 const corsOption = {
-    origin: "https://servicenow-kappa.vercel.app",
+    origin: "https://servicenow-backend.vercel.app",
     methods: 'GET, POST, PUT, DELETE, PATCH, HEAD',
     credentials: true
 }
