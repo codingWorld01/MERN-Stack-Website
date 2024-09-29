28c533ab2abe6d665e076ed81db3d2149f586256
Yatharth Aurangpure <106876791+codingWorld01@users.noreply.github.com>
2024-09-29 11:42:59 +0530

    Update AdminUsers.jsx

--- client/src/pages/AdminUsers.jsx
+++ client/src/pages/AdminUsers.jsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom"
 
 export const AdminUsers = () => {
     const { token } = useAuth();
-    const API = import.meta.env.VITE_APP_URI_API;
+    const API = "https://servicenow-backend.vercel.app";
 
     const [data, setData] = useState([]);
     useEffect(() => {
